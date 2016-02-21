62d28cdca7533dccfdc432706cb92d3a4b38539b
王增康 <Charles@wangzengkangdemacbook-pro.local>
2016-02-21 12:17:24 +0800

    Initial Commit

---
+++ PokeGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PokeGame
+//
+//  Created by 王增康 on 16/2/21.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
